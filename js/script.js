dc09bc8be18bbb27435269fc0c785f078e828e2c
Franz <fmhorfilla.wbridges@gmail.com>
2024-03-04 17:51:44 +0800

    update page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
 
-	"use strict";
+    "use strict";
 
-    function isIE () {
+    function isIE() {
         var myNav = navigator.userAgent.toLowerCase();
         return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1], 10) : false;
     }
@@ -45,12 +45,12 @@
 
     function bgParallax() {
         if ($(".parallax").length) {
-            $(".parallax").each(function() {
+            $(".parallax").each(function () {
                 var height = $(this).position().top;
-                var resize     = height - $(window).scrollTop();
+                var resize = height - $(window).scrollTop();
                 var parallaxSpeed = $(this).data("speed");
                 var doParallax = -(resize / parallaxSpeed);
-                var positionValue   = doParallax + "px";
+                var positionValue = doParallax + "px";
                 var img = $(this).data("bg-image");
 
                 $(this).css({
@@ -59,7 +59,7 @@
                     backgroundSize: "cover"
                 });
 
-                if ( window.innerWidth < 768) {
+                if (window.innerWidth < 768) {
                     $(this).css({
                         backgroundPosition: "center center"
                     });
@@ -72,12 +72,12 @@
 
     function sliderBgSetting() {
         if ($(".hero-slider .slide-item").length) {
-            $(".hero-slider .slide-item").each(function() {
+            $(".hero-slider .slide-item").each(function () {
                 var $this = $(this);
                 var img = $this.find(".slider-bg").attr("src");
 
                 $this.css({
-                    backgroundImage: "url("+ img +")",
+                    backgroundImage: "url(" + img + ")",
                     backgroundSize: "cover",
                     backgroundPosition: "center center"
                 })
@@ -121,17 +121,15 @@
     }
 
     function preloader() {
-        if($('.preloader').length) {
-            $('.preloader').delay(10).fadeOut(500, function() {
+        if ($('.preloader').length) {
+            $('.preloader').delay(10).fadeOut(500, function () {
 
-                //active wow
                 wow.init();
 
-                if($(".save-the-date").length) {
+                if ($(".save-the-date").length) {
                     popupSaveTheDateCircle();
                 }
 
-                //Active heor slider
                 heroSlider();
 
             });
@@ -139,33 +137,33 @@
     }
 
     var wow = new WOW({
-        boxClass:     'wow',      // default
-        animateClass: 'animated', // default
-        offset:       0,          // default
-        mobile:       true,       // default
-        live:         true        // default
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 0,
+        mobile: true,
+        live: true
     });
 
     if ($(".gallery-fancybox").length) {
         $(".fancybox").fancybox({
-            openEffect  : "elastic",
-            closeEffect : "elastic",
-            wrapCSS     : "project-fancybox-title-style"
+            openEffect: "elastic",
+            closeEffect: "elastic",
+            wrapCSS: "project-fancybox-title-style"
         });
     }
 
     if ($(".video-play-btn").length) {
-        $(".video-play-btn").on("click", function(){
+        $(".video-play-btn").on("click", function () {
             $.fancybox({
                 href: this.href,
                 type: $(this).data("type"),
-                'title'         : this.title,
-                helpers     : {
-                    title : { type : 'inside' },
-                    media : {}
+                'title': this.title,
+                helpers: {
+                    title: { type: 'inside' },
+                    media: {}
                 },
 
-                beforeShow : function(){
+                beforeShow: function () {
                     $(".fancybox-wrap").addClass("gallery-fancybox");
                 }
             });
@@ -187,7 +185,7 @@
             type: 'image',
 
             gallery: {
-              enabled: true
+                enabled: true
             },
 
             zoom: {
@@ -195,7 +193,7 @@
 
                 duration: 300,
                 easing: 'ease-in-out',
-                opener: function(openerElement) {
+                opener: function (openerElement) {
                     return openerElement.is('img') ? openerElement : openerElement.find('img');
                 }
             }
@@ -210,7 +208,7 @@
 
                 duration: 300,
                 easing: 'ease-in-out',
-                opener: function(openerElement) {
+                opener: function (openerElement) {
                     return openerElement.is('img') ? openerElement : openerElement.find('img');
                 }
             }
@@ -221,7 +219,7 @@
         if ($(".sortable-gallery .gallery-filters").length) {
             var $container = $('.gallery-container');
             $container.isotope({
-                filter:'*',
+                filter: '*',
                 animationOptions: {
                     duration: 750,
                     easing: 'linear',
@@ -229,12 +227,12 @@
                 }
             });
 
-            $(".gallery-filters li a").on("click", function() {
+            $(".gallery-filters li a").on("click", function () {
                 $('.gallery-filters li .current').removeClass('current');
                 $(this).addClass('current');
                 var selector = $(this).attr('data-filter');
                 $container.isotope({
-                    filter:selector,
+                    filter: selector,
                     animationOptions: {
                         duration: 750,
                         easing: 'linear',
@@ -250,13 +248,13 @@
 
     function masonryGridSetting() {
         if ($('.masonry-gallery').length) {
-            var $grid =  $('.masonry-gallery').masonry({
+            var $grid = $('.masonry-gallery').masonry({
                 itemSelector: '.grid',
                 columnWidth: '.grid',
                 percentPosition: true
             });
 
-            $grid.imagesLoaded().progress( function() {
+            $grid.imagesLoaded().progress(function () {
                 $grid.masonry('layout');
             });
         }
@@ -305,12 +303,12 @@
 
         var imgHolder = $(".wedding-couple-section .gb .img-holder");
 
-        imgHolder.each(function() {
+        imgHolder.each(function () {
             var $this = $(this);
             var imgHolderPic = $this.find("img").attr("src");
 
             $this.css({
-                backgroundImage: "url("+ imgHolderPic +")",
+                backgroundImage: "url(" + imgHolderPic + ")",
                 backgroundSize: "cover",
                 backgroundPosition: "center center"
             })
@@ -318,12 +316,12 @@
     }
 
     if ($("#clock").length) {
-        $('#clock').countdown('2024/12/18 13:00:00', function(event) {
+        $('#clock').countdown('2024/12/18 13:00:00', function (event) {
             var $this = $(this).html(event.strftime(''
-            + '<div class="box"><div>%D</div> <span>Days</span> </div>'
-            + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
-            + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
-            + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
+                + '<div class="box"><div>%D</div> <span>Days</span> </div>'
+                + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
+                + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
+                + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
         });
     }
 
@@ -349,13 +347,13 @@
             margin: 20,
             stagePadding: 10,
             responsive: {
-                0 : {
+                0: {
                     items: 1
                 },
-                480 : {
+                480: {
                     items: 2
                 },
-                768 : {
+                768: {
                     items: 3
                 }
             }
@@ -395,18 +393,18 @@
                     url: "mail.php",
                     data: $(form).serialize(),
                     success: function () {
-                        $( "#loader").hide();
-                        $( "#success").slideDown( "slow" );
-                        setTimeout(function() {
-                        $( "#success").slideUp( "slow" );
+                        $("#loader").hide();
+                        $("#success").slideDown("slow");
+                        setTimeout(function () {
+                            $("#success").slideUp("slow");
                         }, 3000);
                         form.reset();
                     },
-                    error: function() {
-                        $( "#loader").hide();
-                        $( "#error").slideDown( "slow" );
-                        setTimeout(function() {
-                        $( "#error").slideUp( "slow" );
+                    error: function () {
+                        $("#loader").hide();
+                        $("#error").slideDown("slow");
+                        setTimeout(function () {
+                            $("#error").slideUp("slow");
                         }, 3000);
                     }
                 });
@@ -416,18 +414,18 @@
         });
     }
 
-    if($(".music-box").length) {
+    if ($(".music-box").length) {
         var musicBtn = $(".music-box-toggle-btn"),
             musicBox = $(".music-holder");
 
-        musicBtn.on("click", function() {
+        musicBtn.on("click", function () {
             musicBox.toggleClass("toggle-music-box");
             return false;
         })
     }
 
-    if($(".back-to-top-btn").length) {
-        $(".back-to-top-btn").on("click", function() {
+    if ($(".back-to-top-btn").length) {
+        $(".back-to-top-btn").on("click", function () {
             $("html,body").animate({
                 scrollTop: 0
             }, 500, "easeInOutExpo");
@@ -440,7 +438,7 @@
             items: 1,
             smartSpeed: 500,
             nav: true,
-            navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
             dots: false
         })
     }
@@ -452,7 +450,7 @@
             perturbance: 0.04,
         });
 
-        setInterval(function() {
+        setInterval(function () {
             var $el = $('.ripple');
             var x = Math.random() * $el.outerWidth();
             var y = Math.random() * $el.outerHeight();
@@ -490,23 +488,19 @@
         });
     }
 
-    if ($(".surface-shader").length) {
-        //$('.surface-shader')
-    }
+    $(window).on('load', function () {
 
-        $(window).on('load', function() {
+        preloader();
 
-            preloader();
+        sliderBgSetting();
 
-            sliderBgSetting();
+        if ($(".wedding-couple-section").length) {
+            setTwoColEqHeight($(".wedding-couple-section .gb .img-holder"), $(".wedding-couple-section .gb .details"));
+        }
 
-            if($(".wedding-couple-section").length) {
-                setTwoColEqHeight($(".wedding-couple-section .gb .img-holder"), $(".wedding-couple-section .gb .details"));
-            }
+    });
 
-        });
-
-    $(window).on("scroll", function() {
+    $(window).on("scroll", function () {
 
         bgParallax();
 
@@ -518,24 +512,24 @@
             stickIt($(".sticky-2"), "sticky-on", 300);
         }
     });
-    window.onscroll = function() {myFunction()};
+    window.onscroll = function () { myFunction() };
 
     var header = document.getElementById("myHeader");
     var sticky = header.offsetTop;
 
     function myFunction() {
-      if (window.pageYOffset > sticky) {
-        header.classList.add("sticky-header");
-      } else {
-        header.classList.remove("sticky-header");
-      }
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky-header");
+        } else {
+            header.classList.remove("sticky-header");
+        }
     }
 
-    $(window).on("resize", function() {
+    $(window).on("resize", function () {
         toggleClassForSmallNav();
 
         clearTimeout($.data(this, 'resizeTimer'));
-        $.data(this, 'resizeTimer', setTimeout(function() {
+        $.data(this, 'resizeTimer', setTimeout(function () {
             smallNavFunctionality();
         }, 200));
     });
